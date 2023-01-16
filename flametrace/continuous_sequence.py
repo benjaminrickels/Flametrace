208e3d3cda22cfebd70b8a81464af9eb8f1e5df0
Benjamin Rickels <rickels@in.tum.de>
2023-01-16 22:59:26 +0100

    Refactor and speed up finding continuous sequences

--- flametrace/continuous_sequence.py
+++ flametrace/continuous_sequence.py
@@ -7,79 +7,61 @@ from flametrace.util import flatten
 ##############################
 
 
-def _has_thread_uid(entry, thread_uid):
-    return trace_entry.thread_uid(entry) == thread_uid
+def _curr_thread(cpu_seq):
+    return trace_entry.thread_uid(cpu_seq['entries'][0])
 
 
-def _is_begin(entry, thread_uid):
-    return _has_thread_uid(entry, thread_uid)
+def _init_cpu_seq(entry, preempted=None):
+    cpu_seq = {'entries': [entry]}
+    if preempted:
+        cpu_seq['preempts'] = preempted
+
+    return cpu_seq
 
 
-def _is_end(entry, thread_uid, cpu_id):
-    if not _has_thread_uid(entry, thread_uid):
-        return trace_entry.cpu_id(entry) == cpu_id
+def _preempt_seq(entry, cpu_seq, cont_seqs):
+    preempted = cpu_seq['entries'][-1]
+    cpu_seq['preempted_by'] = entry
+    cont_seqs.append(cpu_seq)
+
+    return preempted
+
+
+def _process_entry_with_seq(entry, cpu_seq, cont_seqs):
+    thread_uid = trace_entry.thread_uid(entry)
+    cpu_curr_thread = _curr_thread(cpu_seq)
+
+    if cpu_curr_thread != thread_uid:
+        preempted = _preempt_seq(entry, cpu_seq, cont_seqs)
+        cpu_seq = _init_cpu_seq(entry, preempted)
     else:
-        # Our thread suddenly switched CPU. This should not be happening, but better be safe
-        # than sorry...
-        return trace_entry.cpu_id(entry) != cpu_id
+        cpu_seq['entries'].append(entry)
+
+    return cpu_seq
 
 
-def _find_begin(thread_uid, it):
-    return next((i for i, entry in it if _is_begin(entry, thread_uid)), None)
+def _process_entry(entry, cont_seqs, cpu_seqs):
+    cpu_id = entry['cpu_id']
 
+    cpu_seq = cpu_seqs.get(cpu_id, None)
 
-def _find_end(thread_uid, cpu_id, it):
-    return next((i for i, entry in it if _is_end(entry, thread_uid, cpu_id)),
-                None)
-
-
-def _filter_thread_uid(trace_entries, thread_uid):
-    return filter(lambda entry: _has_thread_uid(entry, thread_uid), trace_entries)
-
-
-def _find_next_entries(thread_uid, trace_entries, it):
-    if (begin := _find_begin(thread_uid, it)) is None:
-        return None
-
-    cpu_id = trace_entry.cpu_id(trace_entries[begin])
-
-    end = _find_end(thread_uid, cpu_id, it) or len(trace_entries)
-
-    return list(_filter_thread_uid(trace_entries[begin:end], thread_uid))
-
-
-def _find_preemption_info(seq_entries, trace_entries):
-    return (find_preempted(seq_entries[0], trace_entries),
-            find_preempting(seq_entries[-1], trace_entries))
-
-
-def _with_preemption_info(seq_entries, preemption_info):
-    preempted, preempting = preemption_info
-    return {'entries': seq_entries,
-            'preempts': preempted,
-            'preempted_by': preempting}
-
-
-def _find_next(thread_uid, trace_entries, it):
-    if (entries := _find_next_entries(thread_uid, trace_entries, it)):
-        preemption_info = _find_preemption_info(entries, trace_entries)
-        return _with_preemption_info(entries, preemption_info)
-
-    return None
-
-
-def _find_all_for(thread_uid, trace_entries):
-    cont_seqs = []
-    it = enumerate(trace_entries)
-    while (cont_seq := _find_next(thread_uid, trace_entries, it)):
-        cont_seqs.append(cont_seq)
-
-    return list(cont_seqs)
+    if cpu_seq is None:
+        cpu_seqs[cpu_id] = _init_cpu_seq(entry)
+    else:
+        cpu_seqs[cpu_id] = _process_entry_with_seq(entry, cpu_seq, cont_seqs)
 
 
 def find_all(trace_entries):
-    thread_uids = trace_entry.thread_uids(trace_entries)
-    return flatten([_find_all_for(thread_uid, trace_entries) for thread_uid in thread_uids])
+    cont_seqs = []
+    cpu_seqs = {}
+
+    for entry in trace_entries:
+        _process_entry(entry, cont_seqs, cpu_seqs)
+
+    for cpu_seq in cpu_seqs.values():
+        cont_seqs.append(cpu_seq)
+
+    return cont_seqs
 
 
 ####################################
@@ -96,11 +78,11 @@ def entry(i, cont_seq):
 
 
 def preempted(cont_seq):
-    return cont_seq['preempts']
+    return cont_seq.get('preempts', None)
 
 
 def preempting(cont_seq):
-    return cont_seq['preempted_by']
+    return cont_seq.get('preempted_by', None)
 
 
 def first(cont_seq):
