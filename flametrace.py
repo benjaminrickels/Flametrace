98ac0365a7f651decf575c4d82fa7074446d91d0
Benjamin Rickels <rickels@in.tum.de>
2023-03-16 16:14:10 +0100

    Enable caching of previous calculations

--- flametrace.py
+++ flametrace.py
@@ -11,6 +11,7 @@ import flametrace.limit as Limit
 import json
 import os
 import shutil
+import pickle
 
 
 def _setup_parser():
@@ -27,12 +28,15 @@ def _setup_parser():
 
     parser.add_argument('--cpu-ghz', action='store', type=float,
                         help='CPU frequency in GHz (defaults can be configured in flametrace/config.py)')
+    parser.add_argument('--no-cache', action='store_true', default=False,
+                        help='Do not cache slices for faster consecutive executions')
     parser.add_argument('--no-filter-pre-m5', action='store_true', default=False,
                         help=('do not filter entries from the tracefile that appear before the first entry belonging '
                               'to the m5 thread'))
     parser.add_argument('--no-trace-convert-to-cycles', action='store_true', default=False,
                         help=('do not convert timestamps (in the tracefile) to cycles and keep them in picoseconds'
                               '(defaults can be configured in flametrace/config.py)'))
+    parser.add_argument('--reset-cache', action='store_true', default=False)
     parser.add_argument('--throw', action='store_true', default=False,
                         help=('When specified and more than one tracefile is specified, throw and exit the program at'
                               'the first exception'))
@@ -66,46 +70,92 @@ def _results_dir(tracefile):
 def _setup_results_dir(tracefile):
     dir = _results_dir(tracefile)
 
-    if os.path.exists(dir):
-        shutil.rmtree(dir)
+    if not os.path.exists(dir):
+        os.mkdir(dir)
 
-    os.mkdir(dir)
     os.chdir(dir)
 
 
-def _run1(tracefile_name, args):
+def _try_load_cached():
+    try:
+        with open('cache', 'rb') as cf:
+            return pickle.load(cf)
+    except FileNotFoundError:
+        return None
+
+
+def _try_save_cached(benchmark_events, slices):
+    try:
+        with open('cache', 'wb') as cf:
+            cache = {'benchmark_events': benchmark_events, 'slices': slices}
+            pickle.dump(cache, cf)
+    except FileExistsError:
+        pass
+
+
+def _compute_slices(tf, args):
+    if not (args.no_cache or args.reset_cache):
+        print('INFO: No cached slices found')
+
+    print('INFO: Parsing tracefile')
+    events = None
+    events = tracefile.parse(tf, filter_pre_m5=not args.no_filter_pre_m5)
+
+    benchmark_events = tracefile.benchmark_events(events)
+
+    print('INFO: Building slices')
+    slices = exec_slices.find_all(events)
+
+    if not args.no_cache:
+        _try_save_cached(benchmark_events, slices)
+
+    return (benchmark_events, slices)
+
+
+def _get_slices(tf, args):
+    cached = None if args.no_cache or args.reset_cache else _try_load_cached()
+
+    if not cached:
+        return _compute_slices(tf, args)
+    else:
+        print('INFO: Cached slices loaded')
+
+        benchmark_events = cached['benchmark_events']
+        slices = cached['slices']
+
+    return (benchmark_events, slices)
+
+
+def _run1(tf, tracefile_name, args):
     if os.path.isdir(tracefile_name):
         return
 
     cwd = os.getcwd()
+    _setup_results_dir(tracefile_name)
 
     try:
-        with open(tracefile_name) as tf:
-            events = tracefile.parse(tf, filter_pre_m5=not args.no_filter_pre_m5)
+        benchmark_events, slices = _get_slices(tf, args)
 
-        _setup_results_dir(tracefile_name)
+        if limit := args.limit:
+            print('INFO: Applying limit')
+            slices = exec_slices.limit(slices,
+                                       limit,
+                                       args.limit_context,
+                                       benchmark_events)
 
-        benchmark_events = tracefile.benchmark_events(events)
+        if args.stats:
+            print('INFO: Computing stats')
+            stats_ = stats.compute_stats(slices)
+            for stat_group, group_stats in stats_.items():
+                with open(f'stats-{stat_group}.json', 'w') as sf:
+                    json.dump(group_stats, sf, indent=4)
 
-        if events:
-            slices = exec_slices.find_all(events)
-            if limit := args.limit:
-                slices = exec_slices.limit(slices,
-                                           limit,
-                                           args.limit_context,
-                                           benchmark_events)
+        if args.fg_d3:
+            d3.to_json(slices)
 
-            if args.stats:
-                stats_ = stats.compute_stats(slices)
-                for stat_group, group_stats in stats_.items():
-                    with open(f'stats-{stat_group}.json', 'w') as sf:
-                        json.dump(group_stats, sf, indent=4)
-
-            if args.fg_d3:
-                d3.to_json(slices)
-
-            if args.fg_svg:
-                svg.to_svg(slices, args.fg_svg_width, args.fg_svg_height)
+        if args.fg_svg:
+            print('INFO: Generating SVG')
+            svg.to_svg(slices, args.fg_svg_width, args.fg_svg_height)
     except Exception:
         raise
     finally:
@@ -113,13 +163,15 @@ def _run1(tracefile_name, args):
 
 
 def _try_run1(tracefile, args):
-    try:
-        _run1(tracefile, args)
-        print(f'"{tracefile}" done')
-    except Exception as e:
-        print(f'ERROR: when processing "{tracefile}": {repr(e)}')
-        if len(args.tracefiles) == 1 or args.throw:
-            raise e
+    with open(tracefile, 'r') as tf:
+        try:
+            print(f'INFO: Now processing "{tracefile}"')
+            _run1(tf, tracefile, args)
+            print(f'INFO: "{tracefile}" done')
+        except Exception as e:
+            print(f'ERROR: processing "{tracefile}": {repr(e)}')
+            if len(args.tracefiles) == 1 or args.throw:
+                raise e
 
 
 def main():
